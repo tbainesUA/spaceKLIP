39dc6e048a2b2bf1b9e741c429d523dad48f1243
Tyler Baines <tbaines@stsci.edu>
2026-07-28 15:13:58 -0400

    updated raw module with  new masking module changes

--- spaceKLIP/analysis/contrast/raw.py
+++ spaceKLIP/analysis/contrast/raw.py
@@ -9,11 +9,9 @@ import os
 from pathlib import Path
 
 import astropy.units as u
-import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
 from pyklip import klip
-from scipy.ndimage import rotate
 from stpsf.constants import JWST_CIRCUMSCRIBED_DIAMETER
 
 from spaceKLIP import utils as ut
@@ -21,6 +19,7 @@ from spaceKLIP.psf import get_offsetpsf
 from spaceKLIP.starphot import get_stellar_magnitudes
 from spaceKLIP.utils import set_surrounded_pixels, write_starfile
 
+from .masking import apply_instrument_mask
 from .plotting import plot_masked_data, plot_raw_contrast
 
 log = logging.getLogger(__name__)
@@ -887,140 +886,150 @@ def run_raw_contrast(
             # Mask coronagraph spiders, 4QPM edges, etc.
             debug_bar_mask = True
             print(" --------- I am here -------")
-            if database.red[key]["EXP_TYPE"][j] in ["NRC_CORON"]:
-                # NOTES: TB I am not sure what this is suppose to look like.
-                # The test/code I have for this produces a result but I do not have
-                # a reference to compare it too.
-                if "WB" in database.red[key]["CORONMSK"][j]:
-                    log.info("  Masking out areas for NIRCam bar coronagraph")
-                    xr = np.arange(data.shape[-1]) - center[0]
-                    yr = np.arange(data.shape[-2]) - center[1]
-                    xx, yy = np.meshgrid(xr, yr)
-                    pa = -np.rad2deg(np.arctan2(xx, yy))
-                    pa[pa < 0.0] += 360.0
-                    ww_sci = np.where(database.obs[key]["TYPE"] == "SCI")[0]
-                    for ww in ww_sci:
-                        roll_ref = database.obs[key]["ROLL_REF"][ww]  # deg
-                        pa1 = (90.0 - 15.0 + roll_ref) % 360.0
-                        pa2 = (90.0 + 15.0 + roll_ref) % 360.0
-                        if pa1 > pa2:
-                            temp = (pa > pa1) | (pa < pa2)
-                        else:
-                            temp = (pa > pa1) & (pa < pa2)
-                        data[:, temp] = np.nan
-                        pa1 = (270.0 - 15.0 + roll_ref) % 360.0
-                        pa2 = (270.0 + 15.0 + roll_ref) % 360.0
-                        if pa1 > pa2:
-                            temp = (pa > pa1) | (pa < pa2)
-                        else:
-                            temp = (pa > pa1) & (pa < pa2)
-                        if debug_bar_mask:
-                            plt.figure()
-                            plt.title("NIRCam Bar Mask")
-                            plt.imshow(temp)
-                            plt.show()
-                            debug_bar_mask = False
+            # if database.red[key]["EXP_TYPE"][j] in ["NRC_CORON"]:
+            #     # NOTES: TB I am not sure what this is suppose to look like.
+            #     # The test/code I have for this produces a result but I do not have
+            #     # a reference to compare it too.
+            #     if "WB" in database.red[key]["CORONMSK"][j]:
+            #         log.info("  Masking out areas for NIRCam bar coronagraph")
+            #         xr = np.arange(data.shape[-1]) - center[0]
+            #         yr = np.arange(data.shape[-2]) - center[1]
+            #         xx, yy = np.meshgrid(xr, yr)
+            #         pa = -np.rad2deg(np.arctan2(xx, yy))
+            #         pa[pa < 0.0] += 360.0
+            #         ww_sci = np.where(database.obs[key]["TYPE"] == "SCI")[0]
+            #         for ww in ww_sci:
+            #             roll_ref = database.obs[key]["ROLL_REF"][ww]  # deg
+            #             pa1 = (90.0 - 15.0 + roll_ref) % 360.0
+            #             pa2 = (90.0 + 15.0 + roll_ref) % 360.0
+            #             if pa1 > pa2:
+            #                 temp = (pa > pa1) | (pa < pa2)
+            #             else:
+            #                 temp = (pa > pa1) & (pa < pa2)
+            #             data[:, temp] = np.nan
+            #             pa1 = (270.0 - 15.0 + roll_ref) % 360.0
+            #             pa2 = (270.0 + 15.0 + roll_ref) % 360.0
+            #             if pa1 > pa2:
+            #                 temp = (pa > pa1) | (pa < pa2)
+            #             else:
+            #                 temp = (pa > pa1) & (pa < pa2)
+            #             if debug_bar_mask:
+            #                 plt.figure()
+            #                 plt.title("NIRCam Bar Mask")
+            #                 plt.imshow(temp)
+            #                 plt.show()
+            #                 debug_bar_mask = False
 
-                        data[:, temp] = np.nan
-            elif database.red[key]["EXP_TYPE"][j] in ["MIR_4QPM"]:
-                # This is MIRI 4QPM data, want to mask edges. However, close
-                # to the center you don't have a choice. So, want to use
-                # rectangles with a gap in the center.
-                log.info("  Masking out areas for MIRI 4QPM coronagraph")
+            #             data[:, temp] = np.nan
+            # elif database.red[key]["EXP_TYPE"][j] in ["MIR_4QPM"]:
+            #     # This is MIRI 4QPM data, want to mask edges. However, close
+            #     # to the center you don't have a choice. So, want to use
+            #     # rectangles with a gap in the center.
+            #     log.info("  Masking out areas for MIRI 4QPM coronagraph")
 
-                # Create array and pad slightly
-                nanmask = np.zeros_like(data[0])
-                pad = 5
-                nanmask = np.pad(nanmask, pad)
+            #     # Create array and pad slightly
+            #     nanmask = np.zeros_like(data[0])
+            #     pad = 5
+            #     nanmask = np.pad(nanmask, pad)
 
-                # Upsample array to improve centering.
-                samp = 1  # Upsampling factor
-                nanmask = nanmask.repeat(samp, axis=0).repeat(samp, axis=1)
+            #     # Upsample array to improve centering.
+            #     samp = 1  # Upsampling factor
+            #     nanmask = nanmask.repeat(samp, axis=0).repeat(samp, axis=1)
 
-                # Define rectangle edges
-                rect_width = 10 * samp  # pixels
-                thinrect_width = 2 * samp  # pixels
+            #     # Define rectangle edges
+            #     rect_width = 10 * samp  # pixels
+            #     thinrect_width = 2 * samp  # pixels
 
-                cent_rect = [
-                    (center[0] + pad) * samp,
-                    (center[0] + pad) * samp,
-                    (center[1] + pad) * samp,
-                    (center[1] + pad) * samp,
-                ]
-                rect = [
-                    int(cent_rect[i] - (rect_width / 2 * (-1) ** (i % 2)))
-                    for i in range(4)
-                ]
-                thinrect = [
-                    int(cent_rect[i] - (thinrect_width / 2 * (-1) ** (i % 2)))
-                    for i in range(4)
-                ]
+            #     cent_rect = [
+            #         (center[0] + pad) * samp,
+            #         (center[0] + pad) * samp,
+            #         (center[1] + pad) * samp,
+            #         (center[1] + pad) * samp,
+            #     ]
+            #     rect = [
+            #         int(cent_rect[i] - (rect_width / 2 * (-1) ** (i % 2)))
+            #         for i in range(4)
+            #     ]
+            #     thinrect = [
+            #         int(cent_rect[i] - (thinrect_width / 2 * (-1) ** (i % 2)))
+            #         for i in range(4)
+            #     ]
 
-                # Define circle mask for center
-                circ_rad = 15 * samp  # pixels
-                yarr, xarr = np.ogrid[: nanmask.shape[0], : nanmask.shape[1]]
-                rad_dist = np.sqrt(
-                    (xarr - (center[0] + pad) * samp) ** 2
-                    + (yarr - (center[1] + pad) * samp) ** 2
-                )
-                circ = rad_dist < circ_rad
+            #     # Define circle mask for center
+            #     circ_rad = 15 * samp  # pixels
+            #     yarr, xarr = np.ogrid[: nanmask.shape[0], : nanmask.shape[1]]
+            #     rad_dist = np.sqrt(
+            #         (xarr - (center[0] + pad) * samp) ** 2
+            #         + (yarr - (center[1] + pad) * samp) ** 2
+            #     )
+            #     circ = rad_dist < circ_rad
 
-                # Loop over images
-                ww_sci = np.where(database.obs[key]["TYPE"] == "SCI")[0]
-                for ww in ww_sci:
-                    # Apply cross
-                    roll_ref = database.obs[key]["ROLL_REF"][ww]  # deg
-                    temp = np.zeros_like(nanmask)
-                    temp[:, rect[0] : rect[1]] = 1  # Vertical
-                    temp[rect[2] : rect[3], :] = 1  # Horizontal
+            #     # Loop over images
+            #     ww_sci = np.where(database.obs[key]["TYPE"] == "SCI")[0]
+            #     for ww in ww_sci:
+            #         # Apply cross
+            #         roll_ref = database.obs[key]["ROLL_REF"][ww]  # deg
+            #         temp = np.zeros_like(nanmask)
+            #         temp[:, rect[0] : rect[1]] = 1  # Vertical
+            #         temp[rect[2] : rect[3], :] = 1  # Horizontal
 
-                    # Now ensure center isn't completely masked
-                    temp[circ] = 0
+            #         # Now ensure center isn't completely masked
+            #         temp[circ] = 0
 
-                    # Apply thin cross
-                    temp[:, thinrect[0] : thinrect[1]] = 1  # Vertical
-                    temp[thinrect[2] : thinrect[3], :] = 1  # Horizontal
+            #         # Apply thin cross
+            #         temp[:, thinrect[0] : thinrect[1]] = 1  # Vertical
+            #         temp[thinrect[2] : thinrect[3], :] = 1  # Horizontal
 
-                    # Rotate the array, include fixed rotation of FQPM edges
-                    temp = rotate(temp, 90 - roll_ref + 4.83544897, reshape=False)
-                    nanmask += temp
+            #         # Rotate the array, include fixed rotation of FQPM edges
+            #         temp = rotate(temp, 90 - roll_ref + 4.83544897, reshape=False)
+            #         nanmask += temp
 
-                # If pixel value too high, should be masked, else set to 1.
-                nanmask[nanmask >= 0.5] = np.nan
-                nanmask[nanmask < 0.5] = 1
+            #     # If pixel value too high, should be masked, else set to 1.
+            #     nanmask[nanmask >= 0.5] = np.nan
+            #     nanmask[nanmask < 0.5] = 1
 
-                # Downsample, remove padding, and mask data
-                nanmask = nanmask[::samp, ::samp]
-                nanmask = nanmask[pad:-pad, pad:-pad]
-                nanmask = set_surrounded_pixels(nanmask)
+            #     # Downsample, remove padding, and mask data
+            #     nanmask = nanmask[::samp, ::samp]
+            #     nanmask = nanmask[pad:-pad, pad:-pad]
+            #     nanmask = set_surrounded_pixels(nanmask)
 
-                ####################
-                # MIRI mask (This works I just need to fold it in)
-                ####################
+            #     ####################
+            #     # MIRI mask (This works I just need to fold it in)
+            #     ####################
 
-                # print("running new miri mask")
-                # roll_angles = database.obs[key]["ROLL_REF"][ww_sci]
-                # nanmask = generate_miri_4qpm_mask(
-                #     detector_shape=data[0].shape,
-                #     coronagraph_center_pix=center,
-                #     roll_reference_angles_deg=roll_angles,
-                #     padding_pix=pad,
-                #     upsample_factor=samp,
-                # )
+            #     # print("running new miri mask")
+            #     # roll_angles = database.obs[key]["ROLL_REF"][ww_sci]
+            #     # nanmask = generate_miri_4qpm_mask(
+            #     #     detector_shape=data[0].shape,
+            #     #     coronagraph_center_pix=center,
+            #     #     roll_reference_angles_deg=roll_angles,
+            #     #     padding_pix=pad,
+            #     #     upsample_factor=samp,
+            #     # )
 
-                #
-                print(f"data shape: {data.shape}")
-                print(f"center: {center}")
-                plt.figure()
-                plt.title("MIRI - 4PQM - MASK")
-                plt.imshow(nanmask)
-                plt.axvline(center[0])
-                plt.axhline(center[1])
-                plt.show()
+            #     #
+            #     print(f"data shape: {data.shape}")
+            #     print(f"center: {center}")
+            #     plt.figure()
+            #     plt.title("MIRI - 4PQM - MASK")
+            #     plt.imshow(nanmask)
+            #     plt.axvline(center[0])
+            #     plt.axhline(center[1])
+            #     plt.show()
 
-                data *= nanmask
-            elif database.red[key]["EXP_TYPE"][j] in ["MIR_LYOT"]:
-                raise NotImplementedError()
+            #     data *= nanmask
+            # elif database.red[key]["EXP_TYPE"][j] in ["MIR_LYOT"]:
+            #     raise NotImplementedError()
+
+            data = apply_instrument_mask(
+                data,
+                center=center,
+                exposure_type=database.red[key]["EXP_TYPE"][j],
+                coronagraph_mask=database.red[key]["CORONMSK"][j],
+                observation_type=database.obs[key]["TYPE"],
+                roll_reference_angles=database.obs[key]["ROLL_REF"],
+            )
+            print(data.shape)
 
             ####################
             # Mask companions Step
